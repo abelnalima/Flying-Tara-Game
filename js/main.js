0f0245025309a857a1631e63fc8cbb8c489612d6
abelnalima <abel.na.lima@gmail.com>
2020-12-31 11:06:56 -0300

    Game Loop - Background Looping

--- js/main.js
+++ js/main.js
@@ -5,4 +5,21 @@ function start() {
     $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+    /* Principais variaves do jogo */
+    var jogo = {};
+
+    /* Game Loop */
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+        moveFundo();
+    }
+
+    /* Função que movimenta o Background */
+    function moveFundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position")); //Pega o valor da posição atual do background
+        $("#fundoGame").css("background-position", esquerda-2); //atualiza a posição 1 pixel para a esquerda
+    }
+
 }
